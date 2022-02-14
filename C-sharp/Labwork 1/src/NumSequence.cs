86ee10b9073aed5265fdb86cbdc3493dc4430197
Dmytro <dima.plugaturevchik@gmail.com>
2022-02-14 20:22:40 +0200

    The code was rewritten. Two classes were created, more interaction was provided

---
+++ C-sharp/Labwork 1/src/NumSequence.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace Labwork_1
+{
+    public static class NumSequence
+    {
+        public static int[] ChoosingThePrimSeq()
+        {
+            Console.Write("Enter the type of the primary sequence you want to be formed (desc, asc) or use \"rand\" by default: ");
+            string way = Console.ReadLine();
+            Console.Write("Please, enter the length of the sequnce: ");
+
+            return way.ToLower() switch
+            {
+                "desc" => GenOrderDescSeq(),
+                "asc" => GenOrderSeq(),
+                _ => GenerateRandomNumbersInRange()
+            };
+        }
+
+        private static int[] GenerateRandomNumbersInRange()
+        {
+            Random random = new Random();
+            int.TryParse(Console.ReadLine(), out int n);
+            int[] array = Enumerable.Range(1, n).ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                int j = random.Next(n);
+                int x = array[i];
+                array[i] = array[j];
+                array[j] = x;
+            }
+
+            Console.WriteLine(string.Join(" ", array));
+
+            return array;
+        }
+
+        private static int[] GenOrderDescSeq()
+        {
+            int[] seq = Enumerable.Range(1, Convert.ToInt32(Console.ReadLine())).Reverse().ToArray();
+            Console.WriteLine("The sequence sorted descending:");
+            Console.WriteLine(string.Join(" ", seq));
+
+            return seq;
+        }
+
+        private static int[] GenOrderSeq()
+        {
+            int[] seq = Enumerable.Range(1, Convert.ToInt32(Console.ReadLine())).ToArray();
+            Console.WriteLine($"The sequence sorted ascending:\n{string.Join(" ", seq)}");
+
+            return seq;
+        }
+    }
+}
---
+++ C-sharp/Labwork 1/src/SortAlgorithm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Labwork_1
+{
+    public static class SortAlgorithm
+    {
+        public static void ChooseSortAlgorithm(int[] seq)
+        {
+            Console.WriteLine("Enter the algorithm to sort with (bubble, comb) or close program in another case:");
+            switch (Console.ReadLine().ToLower())
+            {
+                case "bubble":
+                    SortBubble(seq);
+                    break;
+                case "comb":
+                    CombSort(seq);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void SortBubble(int[] seq)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            long comparisons = 0;
+            long swaps = 0;
+            int temp;
+            bool isReady = true;
+
+            for (int y = 0; y < seq.Length - 1 && isReady; y++)
+            {
+                isReady = false;
+                for (int i = 1; i < seq.Length; i++)
+                {
+                    if (seq[i - 1] > seq[i])
+                    {
+                        temp = seq[i];
+                        seq[i] = seq[i - 1];
+                        seq[i - 1] = temp;
+                        swaps++;
+                        isReady = true;
+                    }
+                    comparisons++;
+                }
+            }
+
+            timer.Stop();
+            Console.WriteLine($"The new bubble-sorted sequence:\n{string.Join(" ", seq)}");
+            Console.WriteLine($"It has {comparisons} comparisons and {swaps} swaps");
+            Console.WriteLine($"It takes {timer.Elapsed} time to perform the algorithm");
+        }
+
+        private static void CombSort(int[] array)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            double factor = 1.2473309;
+            int step = array.Length - 1;
+            int swaps = 0;
+            int comparisons = 0;
+
+            while (step >= 1)
+            {
+                for (int i = 0; i + step < array.Length; i++)
+                {
+                    if (array[i] > array[i + step])
+                    {
+                        Swap(ref array[i], ref array[i + step]);
+                        swaps++;
+                    }
+                    comparisons++;
+                }
+
+                step = (int)(step / factor);
+            }
+
+            timer.Stop();
+            Console.WriteLine($"The array sorted in the combining way is:\n{string.Join(" ", array)}");
+            Console.WriteLine($"It has {comparisons} comparisons and {swaps} swaps");
+            Console.WriteLine($"It took {timer.Elapsed} time to perform the algorithm.");
+        }
+
+        private static void Swap(ref int value1, ref int value2)
+        {
+            int temp = value1;
+            value1 = value2;
+            value2 = temp;
+        }
+    }
+}
